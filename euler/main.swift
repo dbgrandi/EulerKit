3b5d8e9bdffcfcb294657e88b8bb2180920bcce6
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-02 23:19:21 -0500

    auto-commit on file save at 2015-01-02 23:19:21

--- euler/main.swift
+++ euler/main.swift
@@ -16,7 +16,7 @@ import Foundation
 //  println("problem name: \(i)")
 //}
 
-let prob = Problem28Iterative() //  as EulerProblem // problems[2] as EulerProblem
+let prob = Problem28() //  as EulerProblem // problems[2] as EulerProblem
 let stopwatch = Stopwatch(name:"problem")
 stopwatch.start()
 prob.run()

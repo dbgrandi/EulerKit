41c1aa6766324e8df574769675b4d4a364fb5b5c
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-03 18:32:39 -0500

    auto-commit on file save at 2015-01-03 18:32:39

--- euler/EulerKit/Sequences.swift
+++ euler/EulerKit/Sequences.swift
@@ -77,24 +77,24 @@ class TriangleNumberSequence: SequenceType {
 // A wrapper to stop a Sequence when it grows to
 // a maximum value.
 //
-class LimitSequence<S: SequenceType, T where T == S.Generator.Element>: SequenceType {
+struct LimitSequence<S: SequenceType, T where T == S.Generator.Element>: SequenceType {
     let test: (Int,T) -> Bool
     let sequence: S
-    var counter:Int
 
     init(sequence:S, test: (Int,T) -> Bool) {
         self.sequence = sequence
         self.test = test
-        self.counter = 0
     }
 
     func generate() -> GeneratorOf<T> {
         var generator = self.sequence.generate()
+        let t = test
+        var counter:Int = 0
 
         return GeneratorOf<T> {
             var next = generator.next()
-            self.counter += 1
-            if next != nil && self.test(self.counter, next!) {
+            counter += 1
+            if next != nil && t(counter, next!) {
                 return next
             }
             return .None

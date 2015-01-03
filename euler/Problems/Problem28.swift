26600c19c6f98e72da9122cce9e884a1a0359826
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-02 22:57:21 -0500

    auto-commit on file save at 2015-01-02 22:57:21

--- euler/Problems/Problem28.swift
+++ euler/Problems/Problem28.swift
@@ -2,6 +2,10 @@ import Foundation
 
 class Problem28 : EulerProblem {
   override func run() {
+    // assume the 1x1 core
+    var nums = [1]
+    // 1 + 3...1001
+    stride(from: 3, through: 1001, by: 2)
     //
   }
 }

e3d124e8a8d42401fb96bf62456ef72b3713887c
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-03 16:55:01 -0500

    auto-commit on file save at 2015-01-03 16:55:01

--- euler/Problems/Problem26.swift
+++ euler/Problems/Problem26.swift
@@ -1,9 +1,1 @@
-//
-//  Problem26.swift
-//  euler
-//
-//  Created by David Grandinetti on 1/3/15.
-//  Copyright (c) 2015 David Grandinetti. All rights reserved.
-//
-
-import Foundation
+class Problem26Iter

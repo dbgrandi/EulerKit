fec6686d1b2a75bb9b7800d92f119e6a142962b2
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-02 23:27:58 -0500

    auto-commit on file save at 2015-01-02 23:27:58

--- euler/Problems/Problem28.swift
+++ euler/Problems/Problem28.swift
@@ -16,11 +16,11 @@ class Problem28Iterative : EulerProblem {
 
 class Problem28 : EulerProblem {
   override func run() {
-    // given a size, find the next 4 diagonal numbers around it
-    let findCorners: Int -> [Int] = { i in
+    // given a size, find the sum of the next 4 diagonal numbers around it
+    let findCorners: Int -> Int = { i in
       let corner = i*i
       let stride = i+1
-      return [corner+stride, corner+(2*stride), corner+(3*stride), corner+4*stride]
+      return [(corner+stride), corner+(2*stride), corner+(3*stride), corner+4*stride].reduce(0,+)
     }
 
     // because we are mapping on the diagonals of the next
@@ -28,8 +28,7 @@ class Problem28 : EulerProblem {
     let s = stride(from:1, through:999, by:2)
 
     // assume the 1x1 core
-    let diagonals = map(s,findCorners).reduce([1],+)
-    let sum = diagonals.reduce(0,+)
+    let sum = map(s,findCorners).reduce(1,+)
     println("sum = \(sum)")
   }
 }

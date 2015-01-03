c85d53cf77b525963698620e33db640638b2291d
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-02 22:55:47 -0500

    auto-commit on file save at 2015-01-02 22:55:47

--- euler/Problems/Problem28.swift
+++ euler/Problems/Problem28.swift
@@ -1,10 +1,7 @@
-
-//
-//  Problem28.swift
-//  euler
-//
-//  Created by David Grandinetti on 1/2/15.
-//  Copyright (c) 2015 David Grandinetti. All rights reserved.
-//
-
 import Foundation
+
+class Problem28 : EulerProblem {
+  override func run() {
+    //
+  }
+}

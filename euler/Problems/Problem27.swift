c208c4c1446567a19a18543bbbed3b0a5e7f12d2
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-02 23:36:25 -0500

    auto-commit on file save at 2015-01-02 23:36:25

--- euler/Problems/Problem27.swift
+++ euler/Problems/Problem27.swift
@@ -1,4 +1,20 @@
 //
+// Euler discovered the remarkable quadratic formula:
+//
+// n² + n + 41
+//
+// It turns out that the formula will produce 40 primes for the
+// consecutive values n = 0 to 39.
+//
+// However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is
+// divisible by 41, and certainly when n = 41, 41² + 41 + 41
+// is clearly divisible by 41.
+//
+//
+// The incredible formula  n² − 79n + 1601 was discovered, which
+// produces 80 primes for the consecutive values n = 0 to 79.
+// The product of the coefficients, −79 and 1601, is −126479.
+//
 // Considering quadratics of the form:
 //
 //  n² + an + b, where |a| < 1000 and |b| < 1000

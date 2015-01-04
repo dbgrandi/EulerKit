4fdf21f9d5e6acd492b371841103b37086b69e16
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-04 10:54:07 -0500

    auto-commit on file save at 2015-01-04 10:54:07

--- euler/main.swift
+++ euler/main.swift
@@ -1,27 +1,8 @@
-//
-//  main.swift
-//  euler
-//
-//  Created by David Grandinetti on 8/27/14.
-//  Copyright (c) 2014 David Grandinetti. All rights reserved.
-//
-
 import Foundation
 
-//EulerProblem.registerProblems()
-
-//var problems = EulerProblem.problems()
-//println("problem count = \(problems.count)")
-//for i in problems {
-//  println("problem name: \(i)")
-//}
-
-let prob = Problem26Iter() //  as EulerProblem // problems[2] as EulerProblem
+let prob = Problem26Iter()
 let stopwatch = Stopwatch(name:"problem")
 stopwatch.start()
 prob.run()
 stopwatch.stop()
 stopwatch.stats()
-
-// read the input args
-

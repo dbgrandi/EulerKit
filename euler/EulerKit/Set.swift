521fae14815cf767f43915201da8925a8a5a6c45
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-04 12:39:56 -0500

    auto-commit on file save at 2015-01-04 12:39:56

--- euler/EulerKit/Set.swift
+++ euler/EulerKit/Set.swift
@@ -1,12 +1,12 @@
 class Set<T:Hashable> {
-  var items = [T]()
+  var items = [T: Bool]()
 
   func count() -> Int {
     return items.count
   }
 
   func reduce<U>(initial:U, combine:(U, T) -> U) -> U {
-    return items.reduce(initial, combine:combine)
+    return items.keys.reduce(initial, combine:combine)
   }
 
   func insert(item:T) {

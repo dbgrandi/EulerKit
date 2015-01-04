9e26c7b7531461ee746d15b7e01e59f391532110
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-04 10:53:35 -0500

    auto-commit on file save at 2015-01-04 10:53:35

--- euler/Problems/Problem26.swift
+++ euler/Problems/Problem26.swift
@@ -20,13 +20,41 @@
 //
 
 class Problem26Iter : EulerProblem {
-
-  func cycleLength(n:Int) -> Int {
-    var remainders = Dictionary<Int,Int>()
-    return 0
-  }
-
+  //
+  // This one needs some explanation.
+  //
+  // Assumptions:
+  // - 1/n will have at most a n-1 length cycle
+  // - If we reverse the search and find a long cycle, we can skip
+  //   a lot of smaller iterations
+  // - we don't care about the answer of the division, only the length
+  // - we can test for repeating if we get a remainder that we have seen before
+  //
+  //
   override func run() {
+    var maxLength = 0;
+    var maxDenominator = 0;
 
+    for i in stride(from:999, through:7, by:-2) {
+      if maxLength >= i { break }
+
+      var remainders = [Int](count:i, repeatedValue:0)
+      var value = 1
+      var position = 0
+
+      while (remainders[value] == 0 && value != 0) {
+        remainders[value] = position++
+        value = (value*10) % i
+      }
+
+      if (position - remainders[value] > maxLength) {
+        maxLength = position - remainders[value]
+        maxDenominator = i
+      }
+    }
+
+    println("maxLength = \(maxLength)")
+    println("maxDenominator = \(maxDenominator)")
   }
+
 }

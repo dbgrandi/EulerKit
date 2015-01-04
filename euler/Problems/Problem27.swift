1268b6fa2e47cc4a8d82a63a807fdab7bb6ffe19
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-04 09:33:54 -0500

    auto-commit on file save at 2015-01-04 09:33:54

--- euler/Problems/Problem27.swift
+++ euler/Problems/Problem27.swift
@@ -96,7 +96,7 @@ class Problem27 : EulerProblem {
   func primesLength(a:Int, b:Int) -> Int {
     let nums = IntSequence(start: 0)
     let limit = LimitSequence(sequence: nums) { self.quadratic($1, a:a, b:b).isPrime() }
-    let g = limit.generate()
+    var g = limit.generate()
     var n = 0
     while let _ = g.next() {
       n++

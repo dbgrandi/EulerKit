e844826628c37c9a689583aaa6a54d8286894ab9
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-04 13:06:46 -0500

    auto-commit on file save at 2015-01-04 13:06:46

--- euler/Problems/Problem29.swift
+++ euler/Problems/Problem29.swift
@@ -19,14 +19,14 @@ import Foundation
 class Problem29 : EulerProblem {
   override func run() {
     // problem finished in 0.00285303592681885 seconds
-    var terms = Dictionary<Double,Int>()
+    var terms = Dictionary<Double,Bool>()
 
     // problem finished in 0.151324987411499 seconds
 //    var terms = Set<Double>()
 
     for a in 2...100 {
       for b in 2...100 {
-        terms[pow(Double(a), Double(b))] = 1
+        terms[pow(Double(a), Double(b))] = true
 
 //        terms.insert(pow(Double(a), Double(b)))
       }

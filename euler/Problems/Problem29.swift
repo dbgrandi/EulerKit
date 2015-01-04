4d7bb7f990372456e99415b737615b1e70c1351a
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-04 11:32:50 -0500

    auto-commit on file save at 2015-01-04 11:32:50

--- euler/Problems/Problem29.swift
+++ euler/Problems/Problem29.swift
@@ -18,8 +18,8 @@ class Problem29 : EulerProblem {
   override func run() {
     var terms = Set<Int>()
 
-    for a in 2...100 {
-      for b in 2...100 {
+    for a in 2...5 {
+      for b in 2...5 {
         terms.insert()
       }
     }

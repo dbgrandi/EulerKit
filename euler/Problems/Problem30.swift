bd9e65e742f771e699442f3512e699e2683e796e
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-04 13:20:01 -0500

    auto-commit on file save at 2015-01-04 13:20:01

--- euler/Problems/Problem30.swift
+++ euler/Problems/Problem30.swift
@@ -1,9 +1,21 @@
 //
-//  Problem30.swift
-//  euler
+// Surprisingly there are only three numbers that can be written
+// as the sum of fourth powers of their digits:
 //
-//  Created by David Grandinetti on 1/4/15.
-//  Copyright (c) 2015 David Grandinetti. All rights reserved.
+// 1634 = 1^4 + 6^4 + 3^4 + 4^4
+// 8208 = 8^4 + 2^4 + 0^4 + 8^4
+// 9474 = 9^4 + 4^4 + 7^4 + 4^4
+//
+// As 1 = 14 is not a sum it is not included.
+//
+// The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+//
+// Find the sum of all the numbers that can be written as the sum
+// of fifth powers of their digits.
 //
 
-import Foundation
+class Problem30 : EulerProblem {
+  override func run() {
+    //
+  }
+}

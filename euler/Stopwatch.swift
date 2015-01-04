72c0422d98c6ef6b09a62ce6d280138f2f4f2af3
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-01-04 10:55:42 -0500

    auto-commit on file save at 2015-01-04 10:55:42

--- euler/Stopwatch.swift
+++ euler/Stopwatch.swift
@@ -1,11 +1,3 @@
-//
-//  Stopwatch.swift
-//  euler
-//
-//  Created by David Grandinetti on 9/2/14.
-//  Copyright (c) 2014 David Grandinetti. All rights reserved.
-//
-
 import Foundation
 
 class Stopwatch {
